35ac99e39f1c4d4e86aeb93dad46b755db543d64
kanishkar10k <114131838+kanishkar10k@users.noreply.github.com>
2024-07-22 16:19:48 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,29 @@
+function red1(){
+    document.getElementById("button1").style.backgroundColor="red";
+    document.getElementById("s1").style.backgroundColor="red";
+    document.getElementById("s1").style.borderColor="red";
+    document.getElementById("button4").style.backgroundColor="green";
+    document.getElementById("s4").style.borderColor="green";
+    document.getElementById("s4").style.backgroundColor="green";
+}
+function red2(){
+    document.getElementById("button2").style.backgroundColor="red";
+    document.getElementById("s2").style.backgroundColor="red";
+    document.getElementById("s2").style.borderColor="red";
+    document.getElementById("button4").style.backgroundColor="green";
+    document.getElementById("s4").style.backgroundColor="green";
+    document.getElementById("s4").style.borderColor="green";
+}
+function red3(){
+    document.getElementById("button3").style.backgroundColor="red";
+    document.getElementById("s3").style.backgroundColor="red";
+    document.getElementById("s3").style.borderColor="red";
+    document.getElementById("button4").style.backgroundColor="green";
+    document.getElementById("s4").style.backgroundColor="green";
+    document.getElementById("s4").style.borderColor="green";
+}
+function green(){
+    document.getElementById("button4").style.backgroundColor="green";
+    document.getElementById("s4").style.backgroundColor="green";
+    document.getElementById("s4").style.borderColor="green";
+}
